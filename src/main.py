a7e158dfe94bf249c24d4af55b2171df6e8643f3
CyberShikei <jacopferreira@gmail.com>
2025-01-14 14:03:24 +0200

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,16 @@
+#!venv/bin/python3
+from textnode import TextNode
+
+
+def main():
+    TEXT = "Hello, World!"
+    TEXT_TYPE = 'normal'
+    URL = "https://www.google.com"
+
+    node_args = (TEXT, TEXT_TYPE, URL)
+
+    print(TextNode(*node_args))
+
+
+if __name__ == "__main__":
+    main()
